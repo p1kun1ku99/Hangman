eccdd60aabace7e279d57b7871a6329530a57de4
p1kun1ku99 <isaac.tannenbaum16@gmail.com>
2026-04-19 18:35:36 -0400

    images and words added

--- hangman.py
+++ hangman.py
@@ -36,6 +36,16 @@ HANGMAN_PICS = ['''
  / \  |
     ====''']
 
+words = 'ant baboon badger bat bear beaver camel cat clam cobra \
+      cougar coyote crow deer dog donkey duck eagle elk ferret \
+      fox frog goat goose gorrilla hawk lion lizard llama mole  \
+      monkey moose mouse mule newt otter owl panda parrot pidgeon python rabbit \
+      ram rat raven rhino salmon seal shark sheep skunk sloth snake \
+      spider stork swan tiger toad trout turkey turtle weasel whale wolf wombat zebra'.split()
+
 for frame in HANGMAN_PICS:
   print(frame)
+
+for word in words:
+  print(word)
   
